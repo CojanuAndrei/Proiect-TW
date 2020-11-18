da48b5f20bd8b51e4aa0dcbf63b611797aa6ecd1
Teodora Ciubotaru <teodora.ciubotaru99@e-uvt.ro>
2020-11-18 18:17:47 +0200

    Added content form, page for article and small style related changes

--- Templates/header.php
+++ Templates/header.php
@@ -1,32 +1,28 @@
 <head>
-
- <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+   
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-blue-grey.css">
-<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans'>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
+<link rel="stylesheet" href="style.css">
 <style type = "text/css">
 
-.background{
-background-image: url("jounal.jpg");
-}
+
+body{ 
+background-color:#c1c1c4 ;
+line-height: 1.8;
+background-size: cover;}
+
 
 .brand{
-backgound: #cbb09c !important;
+backgound: #ff1a1a !important;
 padding: 30px 32px;
 }
 
 .brand-text{
 color: #cbb09c !important;
-}
-
-form{
-max-width: 460px;
-margin: 20px auto;
-padding: 20px;
 }
 
 
@@ -48,23 +44,4 @@ font-family: sans-serif;
 <body class="background">
 
 
-
-<!-- Navbar -->
-<div class="w3-top">
- <div class="w3-bar w3-theme-d2 w3-left-align w3-large">
-  <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-theme-d2" href="javascript:void(0);" onclick="openNav()"><i class="fa fa-bars"></i></a>
- 
-  <div class="w3-dropdown-hover w3-hide-small">
-    
-  </div>
-  <a href="#" class="w3-bar-item w3-button w3-hide-small w3-right w3-padding-large w3-hover-white" title="My Account">
-    
-  </a>
- </div>
-</div>
-
-<!-- Navbar on small screens -->
-<div id="navDemo" class="w3-bar-block w3-theme-d2 w3-hide w3-hide-large w3-hide-medium w3-large">
-  
-</div>
 </body>
--- readonly.php
+++ readonly.php
@@ -1,15 +1,11 @@
 <!DOCTYPE html>
 
 <html>
-
-    <head>
-    
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width-device-width, initial-scale=1.0">
+<?php include('Templates/header.php'); ?> 
+<head>
         <title>Blogger</title>
-        <link rel="stylesheet" href="style.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/fontawesome.css"  />
     </head>
+
     <body>
         <!-- <nav>
             <div class="nav-link">
@@ -20,8 +16,8 @@
             
         </nav> -->
         <section class="header">
-            <h1>Blog name</h1>
-            <h4>Create your ideas</h4>
+            <h1>Be inspired</h1>
+            
         </section>
         <hr>
         <section class="content-container">
@@ -29,21 +25,11 @@
                 <h2>Article</h2>
                 <div class="article">
                     <img src = "Pictures/mountains-draw.png" alt="Image">
-                    <p>Text articol....</p>
-                </div>
+                    
+                
                 <h3>Heading</h3>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                <div class="another-article">
-                    <h3>Heading</h3>
-                    <img src = "Pictures/mountains.jpg" alt="Image">
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                    <button class="btn read-more-btn">Read More</button>
-                </div>
-            </div>
-            <div class="right-content">
-                <div class="avatar-content">
-                    <img src="Pictures/mountain.jpg" alt="Avatar">
-                    <p>Detalii...</p>
+                <a href = "article.php"><button class="btn read-more-btn">Read more</button></a>
                 </div>
             </div>
         </section>
